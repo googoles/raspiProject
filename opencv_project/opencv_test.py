ae476c44044ece5b5d1e29cbb14273b2f270c335
Googoles <googoles1@gmail.com>
2020-04-13 22:10:40 +0900

    Initial commit

---
+++ opencv_project/opencv_test.py
@@ -0,0 +1,29 @@
+# import cv2
+# import numpy as np
+#
+# def showImage():
+#     imgfile = 'images/model.png'
+#     img = cv2.imread(imgfile, cv2.IMREAD_COLOR)
+#
+#     cv2.imshow('model', img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+#
+# showImage()
+
+import cv2
+cap = cv2.VideoCapture(0) #0 or -1
+while cap.isOpened():
+    ret, img = cap.read()
+    if ret:
+        cv2.imshow('camera-0', img)
+        if cv2.waitKey(1) & 0xFF == 27: #esc
+            break
+    else:
+        print('no camera!')
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+# 출처: https://blog.xcoda.net/98?category=586774 [악보쓰는 프로그래머]
